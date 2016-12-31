56b7f8da4a594de9d76f34310365d53bc645f02e
lshlyapnikov <lshlyapnikov@gmail.com>
2016-12-31 00:59:03 -0500

    kind of fun

--- Shape.hs
+++ Shape.hs
@@ -22,17 +22,28 @@ area (Rectangle a b) = a * b
 
 area (Ellipse r1 r2) = pi * r1 * r2
 
--- area (Triangle a b c) = sqrt(p * (p - a) * (p - b) * (p -c))
---   where p = (a + b + c) / 2
 area (Triangle a b c) =
   let p = (a + b + c) / 2
   in sqrt(p * (p - a) * (p - b) * (p -c))
         
--- area (Polygon(v1:v2:v3:vs)) = triArea v1 v2 v3 + area (Polygon(v1:v3:vs))
---   where triArea :: Vertex -> Vertex -> Vertex -> Area
---         triArea x y z = area (Triangle (distance x y) (distance y z) (distance z x))
 area (Polygon(v1:v2:v3:vs)) =
   let triArea :: Vertex -> Vertex -> Vertex -> Area
       triArea x y z = area (Triangle (distance x y) (distance y z) (distance z x))
   in triArea v1 v2 v3 + area (Polygon(v1:v3:vs))
 area (Polygon _) = 0
+
+polygonArea :: Shape -> Area
+
+polygonArea (Polygon (v1:v2:vs)) =
+  let trapezoidArea :: Vertex -> Vertex -> Area
+      trapezoidArea (x1, y1) (x2, y2) =
+        let h = x2 - x1
+            a = y1
+            b = y2
+        in (a + b) * h / 2
+  in trapezoidArea v1 v2 + polygonArea(Polygon (v2:vs))
+polygonArea (Polygon _) = 0
+
+polygonArea _ = 0
+
+
--- ShapeTest.hs
+++ ShapeTest.hs
@@ -10,14 +10,21 @@ main = hspec $ do
   describe "Shape area" $ do
     it "Polygon/Tiangle" $ do
       area(Polygon ((0, 0):(10, 0):(0, 10):[])) `shouldBe` 50
+      area(Polygon [(0, 0), (0, 10), (5, 5)]) `shouldBe` 25
     it "Triangle" $ do
       area(Triangle 10 (sqrt 200) 10) `shouldBe` 50
     it "Polygon/Square" $ do 
       area(Polygon ((0, 0):(10, 0):(10, 10):(0, 10):[])) `shouldBe` 100
+    it "Polygon/Trapezoids" $ do
+      polygonArea(Polygon ((0, 0):(0, 10):(10, 10):(10, 0):[])) `shouldBe` 100
     it "Rectangle" $ do
       area(Rectangle 10 10) `shouldBe` 100
     it "Polygon/Square negative coordinates" $ do
       area(Polygon ((0, 0):(-10, 0):(-10, -10):(0, -10):[])) `shouldBe` 100
     it "Ellipse" $ do
       area(Ellipse 11.1 22.3) `shouldBe` pi * 11.1 * 22.3
+    it "Bowtie polygon area" $ do
+      polygonArea (Polygon [(0, 0), (0, 10), (5, 5), (10, 10), (10, 0), (5, 5), (0, 0)]) `shouldBe` 50
+      
+
 
